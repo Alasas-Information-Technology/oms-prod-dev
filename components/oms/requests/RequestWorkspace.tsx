2d53cab9023c1287def9be46d62f0bc0f4fb57d9
Suyog Pazhayapurayil <suyogsanjeevanpp@gmail.com>
2026-09-01 10:33:16 +0530

    refactor: optimize RequestsTable responsiveness by removing colgroup and implementing min-width constraints

--- components/oms/requests/RequestWorkspace.tsx
+++ components/oms/requests/RequestWorkspace.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import { Plus } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
